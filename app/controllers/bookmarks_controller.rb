b85eacb438a0c2bacca218c6a9c109338ed4cac4
MarcAntoine Guerin <marcantoine.guerin2@gmail.com>
2023-05-26 11:10:18 +0200

    rails watch list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,24 +1,32 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
   def new
     @bookmark = Bookmark.new
-    @list = List.find(@params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path_bookmarks_path
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    bookmark.destroy
-    redirect_to bookmarks_path, status: :see_other
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path(@bookmark.list), status: :see_other
   end
 
   private
 
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
   def bookmark_params
-    params.required(:bookmark).permit(:name)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
